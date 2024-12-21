3329133d9ab235fd15b01855949e9dbad7b016d5
Burak SALCAN <buraksalcan@gmail.com>
2024-12-22 01:09:38 +0300

    22.12.2024

---
+++ DovizKurlariDataTable/DovizKurlariDataTable/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DovizKurlariDataTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV_kurlar = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_kurlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV_kurlar
+            // 
+            this.DGV_kurlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_kurlar.Location = new System.Drawing.Point(13, 13);
+            this.DGV_kurlar.Name = "DGV_kurlar";
+            this.DGV_kurlar.RowHeadersWidth = 51;
+            this.DGV_kurlar.RowTemplate.Height = 24;
+            this.DGV_kurlar.Size = new System.Drawing.Size(620, 519);
+            this.DGV_kurlar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.DGV_kurlar);
+            this.Name = "Form1";
+            this.Text = "Döviz Kurları";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_kurlar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV_kurlar;
+    }
+}
+
---
+++ DovizKurlariDataTable/DovizKurlariDataTable/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DovizKurlariDataTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XDocument kurlar = XDocument.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+            XElement rootElement = kurlar.Root;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Kod");
+            dt.Columns.Add("Isim");
+            dt.Columns.Add("Alış");
+            dt.Columns.Add("Satış");
+            foreach (XElement kur in rootElement.Elements())
+            {
+                string kod = kur.Attribute("Kod").Value;
+                string isim = kur.Element("Isim").Value;
+                //double satis = !string.IsNullOrEmpty(kur.Element("ForexSelling").Value) ? Convert.ToDouble(kur.Element("ForexSelling").Value.Replace('.',',')): 0;
+                int unit = Convert.ToInt32(kur.Element("Unit").Value);
+                double satis = 0;
+                if (!string.IsNullOrEmpty(kur.Element("ForexSelling").Value))
+                {
+                    string strSatis = kur.Element("ForexSelling").Value.Replace('.', ',');
+                    satis = Convert.ToDouble(strSatis) / unit;
+                }
+
+                double alis = 0;
+                if (!string.IsNullOrEmpty(kur.Element("ForexBuying").Value))
+                {
+                    string strAlis = kur.Element("ForexBuying").Value.Replace('.', ',');
+                    alis = Convert.ToDouble(strAlis) / unit;
+                }
+                dt.Rows.Add(kod, isim, satis, alis);
+            }
+            DGV_kurlar.DataSource = dt;
+            
+
+        }
+    }
+}
